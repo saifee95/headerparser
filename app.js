4b02a22768fab2cee3af71599745f96c7e1d8bb1
mohammad <msaifee95@gmail.com>
2016-05-16 12:08:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
+     	var info = {
+         'ip-address': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+     	};
+		res.send(info);
+});
+
+module.exports = router;
